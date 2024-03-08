9ca6643170fcd0cac0d533b954105957d77cbb33
sgc93 <smachewgedefaw@gmail.com>
2024-03-08 06:54:50 -0900

    add map from react leaflet package

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,27 @@
+import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer } from "react-leaflet";
+import Header from "../components/Header";
+
 export default function HomePage() {
+	const center = [11.5975972, 37.3941337];
+	const zoom = 30;
 	return (
-		<section className="w-screen h-screen bg-stone-400 text-stone-900 font-bold text-xl p-[3rem]">
-			HomePage
+		<section className="relative w-screen h-screen">
+			<Header />
+			<div
+				id="map"
+				style={{
+					height: "100dvh",
+				}}
+			>
+				<MapContainer center={center} zoom={zoom} style={{ height: "100%" }}>
+					<img src="/map.png" alt="" />
+					<TileLayer
+						attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+						url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+					/>
+				</MapContainer>
+			</div>
 		</section>
 	);
 }
