384c6539ee761fadadec514caf1db574af21704d
sgc93 <smachewgedefaw@gmail.com>
2024-03-08 06:55:52 -0900

    create a component to contain links of different navigations

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+export default function NavBar() {
+	return (
+		<ul className="glassmorphism flex items-center gap-4 text-xl text-stone-900">
+			<li>
+				<button>Offices</button>
+			</li>
+			<li>
+				<button>Infrastructures</button>
+			</li>
+			<li>
+				<button>Others</button>
+			</li>
+		</ul>
+	);
+}
