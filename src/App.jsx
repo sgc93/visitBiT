b7e1017c6e2239d8bf2d17b06f638c0dcd620118
sgc93 <smachewgedefaw@gmail.com>
2024-03-08 21:36:26 -0900

    start using react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <HomePage />,
+	},
+	{
+		path: "*",
+		element: <h1>Not Found : 404</h1>,
+	},
+]);
+
 function App() {
-	return (
-		<section>
-			<HomePage />
-		</section>
-	);
+	return <RouterProvider router={router} />;
 }
 
 export default App;
