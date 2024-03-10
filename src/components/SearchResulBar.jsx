07c6703ae19c3ee8eab0e439a8e60641b88b0e9f
sgc93 <smachewgedefaw@gmail.com>
2024-03-10 11:34:59 -0900

    create a separate component to show search results

---
+++ src/components/SearchResulBar.jsx
@@ -0,0 +1,12 @@
+export default function SearchResulBar() {
+	return (
+		<section className="glassmorphism rounded-r-lg h-full w-[18dvw] px-2 py-2">
+			<input
+				type="text"
+				placeholder="Search here ... "
+				className="w-[100%] rounded-lg border-none outline-none px-3 py-[0.35rem] text-stone-900 font-semibold placeholder:text-stone-500 placeholder:font-semibold focus:backdrop-blur-xl "
+			/>
+			SideBar
+		</section>
+	);
+}
