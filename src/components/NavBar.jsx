8c84602e0fbfc1f23c1d6178e85f33843f35df39
sgc93 <smachewgedefaw@gmail.com>
2024-03-09 22:13:23 -0900

    create header tabs dynamically

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,38 @@
+const tabs = [
+	{
+		text: "Offices",
+		type: "multi",
+	},
+	{
+		text: "Dormitories",
+		type: "mono",
+	},
+	{
+		text: "Wifi Centers",
+		type: "mono",
+	},
+	{
+		text: "Where To Test?",
+		type: "multi", // male toilet , female toilet
+	},
+	{
+		text: "Where To Eat?", // cafe, launch, mother houses
+		type: "multi",
+	},
+	{
+		text: "Where To Drink?",
+		type: "multi",
+	},
+];
+
 export default function NavBar() {
 	return (
-		<ul className="glassmorphism flex items-center gap-4 text-xl text-stone-900">
-			<li>
-				<button>Offices</button>
-			</li>
-			<li>
-				<button>Infrastructures</button>
-			</li>
-			<li>
-				<button>Others</button>
-			</li>
+		<ul className=" flex items-center gap-4 text-xl text-stone-900">
+			{tabs.map((tab) => (
+				<li className="glassmorphism" key={tab.text}>
+					<button>{tab.text}</button>
+				</li>
+			))}
 		</ul>
 	);
 }
