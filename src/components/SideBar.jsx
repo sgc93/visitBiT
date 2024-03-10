0e90a3bf483a693dbe4cf2d94769932a9c1c0eeb
sgc93 <smachewgedefaw@gmail.com>
2024-03-09 22:13:50 -0900

    create sidebar component

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,12 @@
+export default function SideBar() {
+	return (
+		<section className="glassmorphism h-[96%] w-[20%] absolute top-4 left-4 z-[999]">
+			<input
+				type="text"
+				placeholder="Search here ... "
+				className="w-[100%] rounded-lg border-none outline-none px-3 py-[0.35rem] text-stone-900 font-semibold placeholder:text-stone-500 placeholder:font-semibold focus:backdrop-blur-xl "
+			/>
+			SideBar
+		</section>
+	);
+}
