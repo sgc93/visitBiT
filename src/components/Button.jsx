6f634c00eee6e19efae9bd5aa9dde5d9361119a5
sgc93 <smachewgedefaw@gmail.com>
2024-03-10 13:13:29 -0900

    update contenting style

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,12 @@
-export default function Button({ title, handleClick }) {
+export default function Button({ children, title, style, handleClick }) {
 	return (
 		<button
-			className="glassmorphism px-[0.5rem] py-[0.1rem] lowercase font-sans text-lg"
+			className="glassmorphism rounded-lg px-[0.5rem] py-[0.1rem] lowercase font-sans text-lg"
+			style={style}
 			onClick={handleClick}
 		>
 			{title}
+			{children}
 		</button>
 	);
 }
