98e67fe4dfd1b0132fad4aab540df39d8d476985
sgc93 <smachewgedefaw@gmail.com>
2024-03-10 13:14:40 -0900

    handle showing and hiding of search reasult displayer box

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,12 +1,25 @@
-import { IoIosArrowBack } from "react-icons/io";
+import { useState } from "react";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import Button from "./Button";
 import SearchResulBar from "./SearchResulBar";
 
 export default function SideBar() {
+	const [showSearchBar, setShowSearchBar] = useState(true);
+	const [query, setQuery] = useState("");
 	return (
 		<section className="flex items-center gap-1 h-[96dvh] w-[30dvw] absolute top-4 left-0 z-[999]">
 			<section className="glassmorphism  w-[5dvw] h-full"></section>
-			<SearchResulBar />
-			<IoIosArrowBack className="glassmorphism text-[1.3rem] text-stone-600 rounded-sm" />
+			{showSearchBar && <SearchResulBar query={query} setQuery={setQuery} />}
+			{query && (
+				<Button
+					handleClick={() =>
+						setShowSearchBar((showSearchBar) => !showSearchBar)
+					}
+					style={{ opacity: ".7" }}
+				>
+					{showSearchBar ? <IoIosArrowBack /> : <IoIosArrowForward />}
+				</Button>
+			)}
 		</section>
 	);
 }
