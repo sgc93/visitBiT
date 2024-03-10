6902bb31f5a82bbee43e176e9703678709d8cc89
sgc93 <smachewgedefaw@gmail.com>
2024-03-09 22:14:24 -0900

    move map renderer logic to separate component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,27 +1,14 @@
 import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer } from "react-leaflet";
 import Header from "../components/Header";
+import Map from "../components/Map";
+import SideBar from "../components/SideBar";
 
 export default function HomePage() {
-	const center = [11.5975972, 37.3941337];
-	const zoom = 30;
 	return (
 		<section className="relative w-screen h-screen">
 			<Header />
-			<div
-				id="map"
-				style={{
-					height: "100dvh",
-				}}
-			>
-				<MapContainer center={center} zoom={zoom} style={{ height: "100%" }}>
-					<img src="/map.png" alt="" />
-					<TileLayer
-						attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-						url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
-					/>
-				</MapContainer>
-			</div>
+			<SideBar />
+			<Map />
 		</section>
 	);
 }
