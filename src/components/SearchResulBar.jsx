2627ffe90b8247bbb884bf7a1a4951ecccb26952
sgc93 <smachewgedefaw@gmail.com>
2024-03-10 13:14:06 -0900

    let react to control the input element of searchbar

--- src/components/SearchResulBar.jsx
+++ src/components/SearchResulBar.jsx
@@ -1,10 +1,12 @@
-export default function SearchResulBar() {
+export default function SearchResulBar({ query, setQuery }) {
 	return (
 		<section className="glassmorphism rounded-r-lg h-full w-[18dvw] px-2 py-2">
 			<input
 				type="text"
 				placeholder="Search here ... "
 				className="w-[100%] rounded-lg border-none outline-none px-3 py-[0.35rem] text-stone-900 font-semibold placeholder:text-stone-500 placeholder:font-semibold focus:backdrop-blur-xl "
+				value={query}
+				onChange={(e) => setQuery(e.target.value)}
 			/>
 			SideBar
 		</section>
