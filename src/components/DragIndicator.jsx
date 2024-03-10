38b5b20e176883f00871116920064878243b570f
sgc93 <smachewgedefaw@gmail.com>
2024-03-10 10:41:02 -0900

    handling show and hide drag indicator of the nav bar while hovering over the nav bar

--- src/components/DragIndicator.jsx
+++ src/components/DragIndicator.jsx
@@ -1,5 +1,11 @@
 import { MdDragIndicator } from "react-icons/md";
 
-export default function DragIndicator({ className, isShown }) {
-	return isShown && <MdDragIndicator className={className} />;
+export default function DragIndicator({ isShown }) {
+	return (
+		<MdDragIndicator
+			className={`text-[1.7rem] transition-all duration-500 ${
+				isShown ? "opacity-70" : "opacity-0"
+			}`}
+		/>
+	);
 }
