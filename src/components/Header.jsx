afad820f234f9ccb3820c0dac279a1c9cf8c5403
sgc93 <smachewgedefaw@gmail.com>
2024-03-09 22:12:36 -0900

    move the search field to sidebar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,9 @@
-import Logo from "./Logo";
 import NavBar from "./NavBar";
 
 export default function Header() {
 	return (
-		<div className="absolute top-[2rem] left-[2rem] z-[1000] flex items-center gap-8">
-			<Logo />
+		<div className="absolute top-4 left-96 z-[1000] flex items-center gap-8">
 			<NavBar />
-			<input
-				type="text"
-				placeholder="Search here ... "
-				className="glassmorphism border-none w-64 outline-none px-3 py-[0.35rem] text-stone-900 font-semibold placeholder:text-stone-500 placeholder:font-semibold focus:backdrop-blur-xl "
-			/>
 		</div>
 	);
 }
