62f162533b7f1df95fa4003b5ea83933b203796b
sgc93 <smachewgedefaw@gmail.com>
2024-03-10 10:18:49 -0900

    handle draggablity of top navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,25 @@
+import { motion } from "framer-motion";
+import { useState } from "react";
+import DragIndicator from "./DragIndicator";
 import NavBar from "./NavBar";
 
 export default function Header() {
+	const [isDragIndicatorShow, setIsDragIndicatorShown] = useState(false);
+
 	return (
-		<div className="absolute top-4 left-96 z-[1000] flex items-center gap-8">
+		<motion.div
+			className="absolute top-4 left-96 z-[999] flex items-center"
+			drag
+			dragElastic={0.3}
+			onHoverStart={() => setIsDragIndicatorShown(true)}
+			onHoverEnd={() => setIsDragIndicatorShown(false)}
+		>
 			<NavBar />
-		</div>
+			<DragIndicator
+				isShown={isDragIndicatorShow}
+				className={" text-[1.7rem] opacity-70"}
+				message={"draggable"}
+			/>
+		</motion.div>
 	);
 }
