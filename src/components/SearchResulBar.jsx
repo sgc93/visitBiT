a83eb221280704df9dc65d35a4a448ba55c73ce4
sgc93 <smachewgedefaw@gmail.com>
2024-03-11 01:31:13 -0900

    mke a search bar and search result displayer component

--- src/components/SearchResulBar.jsx
+++ src/components/SearchResulBar.jsx
@@ -1,14 +1,33 @@
-export default function SearchResulBar({ query, setQuery }) {
+import { useState } from "react";
+import { MdMenu, MdSearch } from "react-icons/md";
+
+export default function SearchResulBar() {
+	const [query, setQuery] = useState("");
+
 	return (
-		<section className="glassmorphism rounded-r-lg h-full w-[18dvw] px-2 py-2">
-			<input
-				type="text"
-				placeholder="Search here ... "
-				className="w-[100%] rounded-lg border-none outline-none px-3 py-[0.35rem] text-stone-900 font-semibold placeholder:text-stone-500 placeholder:font-semibold focus:backdrop-blur-xl "
-				value={query}
-				onChange={(e) => setQuery(e.target.value)}
-			/>
-			SideBar
+		<section className="flex flex-col items-center gap-2">
+			<div className="flex items-center gap-2">
+				<MdMenu className="text-3xl" />
+				<input
+					type="text"
+					placeholder="Search here ... "
+					className="w-55 rounded-lg border-none outline-none px-3 py-[0.35rem] text-stone-900 font-semibold placeholder:text-stone-500 placeholder:font-semibold focus:backdrop-blur-xl "
+					value={query}
+					onChange={(e) => setQuery(e.target.value)}
+				/>
+				<MdSearch className="text-3xl" />
+			</div>
+			{query && (
+				<div
+					className={`glassmorphism rounded-r-lg w-full px-2 py-2 ${
+						query ? "h-[90.4dvh]" : ""
+					}`}
+				>
+					{query && (
+						<div className=" w-full flex items-center">results for {query}</div>
+					)}
+				</div>
+			)}
 		</section>
 	);
 }
