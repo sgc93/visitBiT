538cefe76557568173c1515aec81327780c140fc
sgc93 <smachewgedefaw@gmail.com>
2024-03-10 23:41:22 -0900

    update content of tab object : add dropdown tabs in tha navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,40 +1,131 @@
+import { HiOfficeBuilding } from "react-icons/hi";
+import { RiArrowDropDownLine } from "react-icons/ri";
 import Button from "./Button";
+import DropDown from "./DropDown";
 
 const tabs = [
 	{
 		text: "Offices",
-		type: "multi",
+		dropdown: true,
+		icon: <HiOfficeBuilding />,
+		dropdownIcon: <RiArrowDropDownLine />,
+		dropdowns: [
+			{
+				text: "faculties",
+				multilevel: true,
+				icon: <HiOfficeBuilding />,
+			},
+			{
+				text: "registrar",
+				multilevel: true,
+				icon: <HiOfficeBuilding />,
+			},
+			{
+				text: "finance",
+				multilevel: true,
+				icon: <HiOfficeBuilding />,
+			},
+		],
 	},
 	{
 		text: "Dormitories",
-		type: "mono",
+		dropdown: false,
 	},
 	{
 		text: "Wifi Centers",
-		type: "mono",
+		dropdown: false,
 	},
 	{
 		text: "Where To Test?",
-		type: "multi", // male toilet , female toilet
+		dropdown: true, // male toilet , female toilet
+		dropdownIcon: <RiArrowDropDownLine />,
+		dropdowns: [
+			{
+				text: "faculties",
+				multilevel: true,
+				icon: <HiOfficeBuilding />,
+			},
+			{
+				text: "registrar",
+				multilevel: true,
+				icon: <HiOfficeBuilding />,
+			},
+			{
+				text: "finance",
+				multilevel: true,
+				icon: <HiOfficeBuilding />,
+			},
+		],
 	},
 	{
 		text: "Where To Eat?", // cafe, launch, mother houses
-		type: "multi",
+		dropdown: true,
+		dropdownIcon: <RiArrowDropDownLine />,
+		dropdowns: [
+			{
+				text: "faculties",
+				multilevel: true,
+				icon: <HiOfficeBuilding />,
+			},
+			{
+				text: "registrar",
+				multilevel: true,
+				icon: <HiOfficeBuilding />,
+			},
+			{
+				text: "finance",
+				multilevel: true,
+				icon: <HiOfficeBuilding />,
+			},
+		],
 	},
 	{
 		text: "Where To Drink?",
-		type: "multi",
+		dropdown: true,
+		dropdownIcon: <RiArrowDropDownLine />,
+		dropdowns: [
+			{
+				text: "faculties",
+				multilevel: true,
+				icon: <HiOfficeBuilding />,
+			},
+			{
+				text: "registrar",
+				multilevel: true,
+				icon: <HiOfficeBuilding />,
+			},
+			{
+				text: "finance",
+				multilevel: true,
+				icon: <HiOfficeBuilding />,
+			},
+		],
 	},
 ];
 
 export default function NavBar() {
+	function handleClick(tab) {
+		console.log("clicked: " + tab);
+	}
+
 	return (
-		<ul className=" flex items-center gap-4 ">
-			{tabs.map((tab) => (
-				<li key={tab.text}>
-					<Button title={tab.text} />
-				</li>
-			))}
+		<ul className=" flex items-start gap-4 ">
+			{tabs.map((tab) =>
+				tab.dropdown ? (
+					<DropDown key={tab.text} tab={tab} />
+				) : (
+					<li key={tab.text}>
+						<Button
+							className="flex items-center gap-2"
+							handleClick={() => handleClick(tab)}
+						>
+							{tab.icon}
+							{tab.text}
+							{tab.dropdown && tab.dropdownIcon}
+						</Button>
+					</li>
+				)
+			)}
 		</ul>
 	);
 }
