86bde3392c9cc951d2a6cee67c97ae1613a2e439
sgc93 <smachewgedefaw@gmail.com>
2024-03-10 23:39:54 -0900

    update contents of Button component to be displayed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,13 @@
-export default function Button({ children, title, style, handleClick }) {
+export default function Button({
+	children,
+	className,
+	title,
+	style,
+	handleClick,
+}) {
 	return (
 		<button
-			className="glassmorphism rounded-lg px-[0.5rem] py-[0.1rem] lowercase font-sans text-lg"
+			className={`glassmorphism rounded-lg px-[0.5rem] py-[0.1rem] lowercase font-sans text-lg ${className}`}
 			style={style}
 			onClick={handleClick}
 		>
