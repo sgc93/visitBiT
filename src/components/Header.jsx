9ba100a493fa08f7da358f519387be31e6158968
sgc93 <smachewgedefaw@gmail.com>
2024-03-11 02:26:15 -0900

    separate search bar from nav bar : give it to sidebar component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,8 @@
 import NavBar from "./NavBar";
-import SearchResulBar from "./SearchResulBar";
 
 export default function Header({ dragConstraint }) {
 	return (
-		<div className="absolute top-4 left-4 z-[999] flex items-start gap-2">
-			<SearchResulBar />
+		<div className="absolute top-4 left-72 z-[999] flex items-start gap-2">
 			<NavBar dragConstraint={dragConstraint} />
 		</div>
 	);
