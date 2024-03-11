1ede2d42920ad45a8bdc356148f418ee71773dae
sgc93 <smachewgedefaw@gmail.com>
2024-03-11 02:28:49 -0900

    clean code : removing unused old texting lines of code

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,25 +1,13 @@
 import { useState } from "react";
-import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
-import Button from "./Button";
 import SearchResulBar from "./SearchResulBar";
 
 export default function SideBar() {
-	const [showSearchBar, setShowSearchBar] = useState(true);
 	const [query, setQuery] = useState("");
+
 	return (
-		<section className="flex items-center gap-1 h-[96dvh] w-[30dvw] absolute top-4 left-0 z-[999]">
-			<section className="glassmorphism  w-[5dvw] h-full"></section>
-			{showSearchBar && <SearchResulBar query={query} setQuery={setQuery} />}
-			{query && (
-				<Button
-					handleClick={() =>
-						setShowSearchBar((showSearchBar) => !showSearchBar)
-					}
-					style={{ opacity: ".7" }}
-				>
-					{showSearchBar ? <IoIosArrowBack /> : <IoIosArrowForward />}
-				</Button>
-			)}
+		<section className="flex gap-1  absolute top-4 left-4 z-[999]">
+			{/* <section className="glassmorphism  w-[5dvw] h-[96dvh]"></section> */}
+			<SearchResulBar query={query} setQuery={setQuery} />
 		</section>
 	);
 }
