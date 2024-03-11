28fc1dafdf78c4a8e98adbb559334e355b4071fb
sgc93 <smachewgedefaw@gmail.com>
2024-03-11 02:42:54 -0900

    handle displaying sidebar menu conditionally

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -3,11 +3,18 @@ import SearchResulBar from "./SearchResulBar";
 
 export default function SideBar() {
 	const [query, setQuery] = useState("");
-
+	const [showSideBar, setShowSideBar] = useState(false);
 	return (
-		<section className="flex gap-1  absolute top-4 left-4 z-[999]">
-			{/* <section className="glassmorphism  w-[5dvw] h-[96dvh]"></section> */}
-			<SearchResulBar query={query} setQuery={setQuery} />
+		<section className="flex flex-col gap-3 absolute top-4 left-4 z-[999]">
+			<SearchResulBar
+				showSideBar={showSideBar}
+				setShowSideBar={setShowSideBar}
+				query={query}
+				setQuery={setQuery}
+			/>
+			{showSideBar && (
+				<section className="glassmorphism rounded-r-lg w-[5dvw] h-[90dvh]"></section>
+			)}
 		</section>
 	);
 }
