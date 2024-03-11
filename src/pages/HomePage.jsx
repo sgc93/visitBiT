11e36bfb63532d7a1641024e4020e63a7060b421
sgc93 <smachewgedefaw@gmail.com>
2024-03-10 23:58:39 -0900

    use the entire homepage as a drag constraint for header component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,17 @@
 import "leaflet/dist/leaflet.css";
+import { useRef } from "react";
 import Header from "../components/Header";
 import Map from "../components/Map";
 import SideBar from "../components/SideBar";
 
 export default function HomePage() {
+	const dragConstraint = useRef();
 	return (
-		<section className="relative w-screen h-screen">
-			<Header />
+		<section
+			className="relative overflow-hidden w-screen h-screen"
+			ref={dragConstraint}
+		>
+			<Header dragConstraint={dragConstraint} />
 			<SideBar />
 			<Map />
 		</section>
