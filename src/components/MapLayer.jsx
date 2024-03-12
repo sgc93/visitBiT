05e7cbba8d71c308202c0939eb948244997ac53e
sgc93 <smachewgedefaw@gmail.com>
2024-03-12 11:43:36 -0900

    create and style map layer changing component

---
+++ src/components/MapLayer.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import { FaHandHoldingWater, FaRestroom, FaSatellite } from "react-icons/fa";
+import { GrLayer, GrLocation } from "react-icons/gr";
+import Button from "./Button";
+
+const className = "opacity-70";
+
+const tabs = [
+	{
+		text: "default",
+		icon: <GrLocation className={className} />,
+	},
+	{
+		text: "hybrid",
+		icon: <FaHandHoldingWater className={className} />,
+	},
+	{
+		text: "satellite",
+		icon: <FaSatellite className={className} />,
+	},
+	{
+		text: "terrain",
+		icon: <FaRestroom className={className} />,
+	},
+];
+
+export default function MapLayer() {
+	const [isOpen, setIsOpen] = useState(false);
+	return (
+		<div
+			className="absolute bottom-4 right-5 z-[999] flex items-end gap-2"
+			onMouseEnter={() => setIsOpen(true)}
+			onMouseLeave={() => setIsOpen(false)}
+		>
+			{isOpen && (
+				<div className="glassmorphism p-1 rounded-l-lg rounded-t-lg flex flex-col gap-1">
+					{tabs.map((tab) => (
+						<Button
+							key={tab.text}
+							className={
+								"flex items-center gap-2 transition-all duration-500 hover:bg-blue-900 hover:text-stone-100"
+							}
+						>
+							{tab.icon}
+							{tab.text}
+						</Button>
+					))}
+				</div>
+			)}
+			<div
+				className={`glassmorphism h-8 px-2 py-1  flex items-center gap-2 text-blue-900 transition-all duration-300 hover:bg-blue-900 hover:text-stone-100 ${
+					isOpen ? "rounded-r-lg" : "rounded-l-lg rounded-b-lg "
+				}`}
+			>
+				<GrLayer />
+				<span
+					className={`transition-all duration-1000 ${
+						!isOpen ? "inline" : "hidden"
+					}`}
+				>
+					layers
+				</span>
+			</div>
+		</div>
+	);
+}
