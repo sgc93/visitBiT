4ff2dfa052f1e4e57ebe8ce421d6bfc30070cc27
sgc93 <smachewgedefaw@gmail.com>
2024-03-12 22:46:32 -0900

    allow changing mark positions

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 import NavBar from "./NavBar";
 
-export default function Header({ dragConstraint }) {
+export default function Header({ dragConstraint, setPositions }) {
 	return (
 		<div className="absolute top-4 left-72 z-[999] flex items-start gap-2">
-			<NavBar dragConstraint={dragConstraint} />
+			<NavBar dragConstraint={dragConstraint} setPositions={setPositions} />
 		</div>
 	);
 }
