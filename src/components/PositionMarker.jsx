2a78168e64ed8aaff8cd9d88bb629d7d8dea9a5d
sgc93 <smachewgedefaw@gmail.com>
2024-03-13 07:30:45 -0900

    style the customized marker

--- src/components/PositionMarker.jsx
+++ src/components/PositionMarker.jsx
@@ -1,10 +1,22 @@
-import React from "react";
 import { Marker, Popup } from "react-leaflet";
+import Button from "./Button";
 
 export default function PositionMarker({ pos }) {
 	return (
 		<Marker position={pos.position}>
-			<Popup>{pos.name}</Popup>
+			<Popup>
+				<div className="flex flex-col gap-2">
+					<span>{pos.name}</span>
+					<img src={pos.img[1]} alt="kitaw_launch" />
+					<Button
+						className={
+							"bg-blue-400 text-sm items-center transition-all duration-300 hover:bg-blue-900 hover:text-stone-100"
+						}
+					>
+						See More
+					</Button>
+				</div>
+			</Popup>
 		</Marker>
 	);
 }
