d892611e35dd29a50e0696512495746f55ab9792
sgc93 <smachewgedefaw@gmail.com>
2024-03-12 22:48:23 -0900

    accept and pass position props

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,19 +1,20 @@
 import "leaflet/dist/leaflet.css";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import Header from "../components/Header";
 import Map from "../components/Map";
 import SideBar from "../components/SideBar";
 
 export default function HomePage() {
+	const [positions, setPositions] = useState([]);
 	const dragConstraint = useRef();
 	return (
 		<section
 			className="relative overflow-hidden w-screen h-screen"
 			ref={dragConstraint}
 		>
-			<Header dragConstraint={dragConstraint} />
+			<Header dragConstraint={dragConstraint} setPositions={setPositions} />
 			<SideBar />
-			<Map />
+			<Map positions={positions} />
 		</section>
 	);
 }
