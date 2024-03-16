c3bcf083e9fba7763d81272d6560492690d50daf
sgc93 <smachewgedefaw@gmail.com>
2024-03-16 11:36:10 -0900

    handle displaying and hiding copy status message : success and error

--- src/components/Copy.jsx
+++ src/components/Copy.jsx
@@ -1,20 +1,38 @@
+import { useEffect, useState } from "react";
 import { IoCopy } from "react-icons/io5";
 
 export default function Copy({ value }) {
+	const [status, setStatus] = useState("");
 	async function handleCopy() {
 		try {
 			await navigator.clipboard.writeText(value);
-			console.log("copied successfully : " + value);
+			setStatus("ok");
 		} catch (error) {
-			console.log("error : " + error);
+			setStatus("error");
 		}
 	}
+
+	useEffect(() => {
+		const id = setTimeout(() => {
+			setStatus("");
+		}, 2000);
+		return () => clearTimeout(id);
+	}, [status]);
+
 	return (
-		<span
-			className="text-md transition-all rounded-full p-1 duration-300 hover:text-stone-100 hover:bg-blue-950 cursor-pointer"
-			onClick={() => handleCopy()}
-		>
-			<IoCopy />
-		</span>
+		<>
+			{status === "" && (
+				<span
+					className="relative text-md transition-all rounded-full p-1 duration-300 hover:text-stone-100 hover:bg-blue-950 cursor-pointer"
+					onClick={() => handleCopy()}
+				>
+					<IoCopy />
+				</span>
+			)}
+			{status === "ok" && (
+				<span className=" text-green-600 font-semibold">copied!</span>
+			)}
+			{status === "error" && <span>not copied!</span>}
+		</>
 	);
 }
