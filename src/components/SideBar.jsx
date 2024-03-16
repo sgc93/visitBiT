d06083127a80e1564e6c87589d5eb3e65f94faf2
sgc93 <smachewgedefaw@gmail.com>
2024-03-16 08:28:23 -0900

    import header part

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
-import SearchResulBar from "./SearchResulBar";
 import SideBarBox from "./SideBarBox";
+import SidebarHeader from "./SideBarHeader";
 
 export default function SideBar() {
 	const [query, setQuery] = useState("");
 	const [showSideBar, setShowSideBar] = useState(false);
 	return (
-		<section className="flex flex-col gap-3 absolute top-4 left-4 z-[999]">
-			<SearchResulBar
+		<section className="flex flex-col gap-3 absolute top-4 left-4 z-[998]">
+			<SidebarHeader
 				showSideBar={showSideBar}
 				setShowSideBar={setShowSideBar}
 				query={query}
