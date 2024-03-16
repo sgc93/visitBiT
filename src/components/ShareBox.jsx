c698d2dda30882a1c03555063022ec74dfc74547
sgc93 <smachewgedefaw@gmail.com>
2024-03-16 11:13:12 -0900

    handle getting users location when permission allowed

--- src/components/ShareBox.jsx
+++ src/components/ShareBox.jsx
@@ -1,9 +1,31 @@
 import { MdClose } from "react-icons/md";
 import Copy from "./Copy";
 
-export default function ShareBox({ place, setShowShareBox }) {
+export default function ShareBox({ place, setShowShareBox, showShareBox }) {
+	const isUserLoc = showShareBox.type === "user";
+	if (isUserLoc) {
+		if ("geolocation" in navigator) {
+			navigator.geolocation.getCurrentPosition(
+				(userPos) => {
+					console.log(userPos);
+					const { latitude, longitude } = userPos.coords;
+					place = {
+						position: [latitude, longitude],
+					};
+				},
+				(error) => {
+					throw new Error(
+						"Something went wrong while locating user : " + error
+					);
+				}
+			);
+		} else {
+			throw new Error("geolocation is not supported in this browser!");
+		}
+	}
+
 	return (
-		<div className="absolute z-[998] left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] glassmorphism px-2 pt-1 pb-4 rounded-lg">
+		<div className=" w-[23rem] absolute z-[998] left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] glassmorphism px-2 pt-1 pb-4 rounded-lg">
 			<div className="w-full flex justify-between items-center gap-1">
 				<span>Share</span>
 				<span className="text-xl text-blue-950 cursor-pointer">
@@ -16,15 +38,17 @@ export default function ShareBox({ place, setShowShareBox }) {
 					/>
 				</span>
 			</div>
-			<div className="p-2 flex items-start gap-2">
-				<img className="w-16 max-h-20" src={place.img[1]} alt={place.name} />
-				<div className="flex flex-col">
-					<span className="text-sm text-blue-950">
-						{place.position[0]},{place.position[1]}
-					</span>
-					<span className="text-lg text-stone-500">{place.name}</span>
+			{!isUserLoc && (
+				<div className="p-2 flex items-start gap-2">
+					<img className="w-16 max-h-20" src={place.img[1]} alt={place.name} />
+					<div className="flex flex-col">
+						<span className="text-sm text-blue-950">
+							{place.position[0]},{place.position[1]}
+						</span>
+						<span className="text-lg text-stone-500">{place.name}</span>
+					</div>
 				</div>
-			</div>
+			)}
 			<div className="flex flex-col gap-1">
 				<span className="text-lg text-stone-600">Share Link</span>
 				<div className="flex items-center gap-2 px-2">
