d77716533467195595cfdb5de6df5e6200386feb
sgc93 <smachewgedefaw@gmail.com>
2024-03-16 10:04:51 -0900

    make a copy btn in separate component

--- src/components/PositionMarker.jsx
+++ src/components/PositionMarker.jsx
@@ -5,6 +5,7 @@ export default function PositionMarker({
 	pos,
 	setShowDetailBox,
 	setDetailedPlace,
+	setShowShareBox,
 }) {
 	return (
 		<Marker position={pos.position}>
@@ -12,17 +13,30 @@ export default function PositionMarker({
 				<div className="flex flex-col gap-2">
 					<span>{pos.name}</span>
 					<img src={pos.img[1]} alt="kitaw_launch" />
-					<Button
-						className={
-							"bg-blue-400 text-sm items-center transition-all duration-300 hover:bg-blue-900 hover:text-stone-100"
-						}
-						handleClick={() => {
-							setShowDetailBox(true);
-							setDetailedPlace(pos);
-						}}
-					>
-						See More
-					</Button>
+					<div className="flex items-center gap-1">
+						<Button
+							className={
+								"bg-blue-400 text-sm items-center transition-all duration-300 hover:bg-blue-900 hover:text-stone-100"
+							}
+							handleClick={() => {
+								setShowDetailBox(true);
+								setDetailedPlace(pos);
+							}}
+						>
+							See More
+						</Button>
+						<Button
+							className={
+								"bg-blue-400 text-sm items-center transition-all duration-300 hover:bg-blue-900 hover:text-stone-100"
+							}
+							handleClick={() => {
+								setDetailedPlace(pos);
+								setShowShareBox(true);
+							}}
+						>
+							share
+						</Button>
+					</div>
 				</div>
 			</Popup>
 		</Marker>
