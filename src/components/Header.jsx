6a20ac982d0f2189dee46c85d35b017fc37c492c
sgc93 <smachewgedefaw@gmail.com>
2024-03-16 08:26:13 -0900

    display quick search bar at header part

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 import NavBar from "./NavBar";
+import QuickSearch from "./QuickSearch";
 
 export default function Header({ dragConstraint, setPositions }) {
 	return (
-		<div className="absolute top-4 left-72 z-[999] flex items-start gap-2">
+		<div className="absolute top-4 left-16 z-[999] flex items-start gap-2">
+			<QuickSearch />
 			<NavBar dragConstraint={dragConstraint} setPositions={setPositions} />
 		</div>
 	);
