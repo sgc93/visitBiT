83f5d5bea8fe331faea38dd139b0b42e877f5032
sgc93 <smachewgedefaw@gmail.com>
2024-03-16 08:29:38 -0900

    remove unused props and states

--- src/components/SideBarHeader.jsx
+++ src/components/SideBarHeader.jsx
@@ -1,16 +1,8 @@
-import { useState } from "react";
 import { MdClose, MdMenu } from "react-icons/md";
 const className =
 	" rounded-md w-2/12 text-2xl transition-all duration-300 hover:bg-blue-900 hover:text-stone-100 hover:opacity-70";
 
-export default function SidebarHeader({
-	showSideBar,
-	setShowSideBar,
-	query,
-	setQuery,
-}) {
-	const [showSearchBar, setShowSearchBar] = useState(true);
-
+export default function SidebarHeader({ showSideBar, setShowSideBar }) {
 	return (
 		<section className="flex flex-col gap-2 w-[18dvw]">
 			<div className="flex items-center gap-2 w-full">
