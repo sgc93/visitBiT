1d2a99aab54997145047f8859c2763ebe1c43d09
sgc93 <smachewgedefaw@gmail.com>
2024-03-16 23:59:01 -0900

    create a helper function to handle cetntering the map and flying to it

--- src/services/helpers.js
+++ src/services/helpers.js
@@ -34,3 +34,22 @@ export function getCurrentPos() {
 		}
 	});
 }
+
+export function getCenter(positions) {
+	const latLng = positions.map((pos) => pos.position);
+	let minLat = Infinity;
+	let maxLat = -Infinity;
+	let minLng = Infinity;
+	let maxLng = -Infinity;
+
+	for (const [lat, lng] of latLng) {
+		minLat = Math.min(minLat, lat);
+		maxLat = Math.max(maxLat, lat);
+		minLng = Math.min(minLng, lng);
+		maxLng = Math.max(maxLng, lng);
+	}
+
+	const center = [(minLat + maxLat) / 2, (minLng + maxLng) / 2];
+	console.log(center);
+	return center;
+}
