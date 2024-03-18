8f5cc34e3e1fee60e9598bdf877e85b660f65931
sgc93 <smachewgedefaw@gmail.com>
2024-03-18 10:35:57 -0900

    create initial profile as a detail data for the campus : bit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,8 +4,19 @@ import Header from "../components/Header";
 import Map from "../components/Map";
 import SideBar from "../components/SideBar";
 
+const bit = [
+	{
+		name: "Bahir Dar Institute Of Technology",
+		description:
+			"a campus of engineering in Bahir Dar University. historically, it is the oldest one - originated in 1963 by Emperor HaileSelassie",
+		position: [11.597621756651337, 37.39551835806901],
+		phone: ["058 220 9338"],
+		img: ["/bit/img1.jpg"],
+	},
+];
+
 export default function HomePage() {
-	const [position, setPosition] = useState([]);
+	const [position, setPosition] = useState(bit);
 	const dragConstraint = useRef();
 
 	function setMarkedPlace(position) {
