4df505fd54f09ba2385c7085c7ac9bebe7dd7d4b
sgc93 <smachewgedefaw@gmail.com>
2024-03-18 23:37:32 -0900

    create a helper function to calculate geographical distance between to places with Haversine Formula - assuming that earth is spherical

--- src/services/helpers.js
+++ src/services/helpers.js
@@ -17,13 +17,7 @@ export function getCurrentPos() {
 			navigator.geolocation.getCurrentPosition(
 				(userPos) => {
 					const { latitude, longitude } = userPos.coords;
-					const pos = [
-						{
-							name: "user",
-							position: [latitude, longitude],
-						},
-					];
-					resolve(pos);
+					resolve([latitude, longitude]);
 				},
 				(error) => {
 					reject("Something went wrong while locating user : " + error);
@@ -52,4 +46,30 @@ export function getCenter(positions) {
 	const center = [(minLat + maxLat) / 2, (minLng + maxLng) / 2];
 	console.log(center);
 	return center;
+}
+
+function inRadian(angle) {
+	return angle * (Math.PI / 180);
+}
+
+// considering that earth is spherical : using Haversine Formula
+export function getDistance(lat1, lng1, lat2, lng2) {
+	const R = 6371; // radius of earth
+	const l1 = inRadian(lat1);
+	const l2 = inRadian(lat2);
+	const dLat = inRadian(lat2 - lat1);
+	const dLng = inRadian(lng2 - lng1);
+
+	// calculate haversine term a
+	const a =
+		Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+		Math.cos(l1) * Math.cos(l2) * Math.sin(dLng / 2) * Math.sin(dLng / 2);
+
+	// calculate central angle c
+	const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+	// calculate distance d = R * c
+	const d = R * c;
+	console.log("distance in km: " + d);
+	return d;
 }
