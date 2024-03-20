c9c0c4b28fb8d8439c053d13e31658f851041320
sgc93 <smachewgedefaw@gmail.com>
2024-03-20 11:30:49 -0900

    handle searching for nearest place

--- src/services/helpers.js
+++ src/services/helpers.js
@@ -102,3 +102,28 @@ export function getNearest(positions, userPos) {
 
 	return nearestPlace;
 }
+
+export function getNearestPlace(positions) {
+	return new Promise((resolve, reject) => {
+		if ("geolocation" in navigator) {
+			navigator.geolocation.getCurrentPosition(
+				(userPos) => {
+					const { latitude, longitude } = userPos.coords;
+
+					resolve([
+						{
+							name: "user",
+							position: [latitude, longitude],
+						},
+						getNearest(positions, [latitude, longitude]),
+					]);
+				},
+				(error) => {
+					reject("Something went wrong while locating user : " + error);
+				}
+			);
+		} else {
+			throw new Error("geolocation is not supported in this browser!");
+		}
+	});
+}
