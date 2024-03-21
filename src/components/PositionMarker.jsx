0b05ad5820bc0c07e691f594fe55f541fe26f6b2
sgc93 <smachewgedefaw@gmail.com>
2024-03-20 23:17:30 -0900

    add tooltips for each places

--- src/components/PositionMarker.jsx
+++ src/components/PositionMarker.jsx
@@ -1,46 +1,54 @@
-import { Marker, Popup } from "react-leaflet";
+import { Marker, Popup, Tooltip } from "react-leaflet";
 import Button from "./Button";
 
+function CustomTip({ props }) {
+	return <div className="glassmorphism">{props.label}</div>;
+}
+
 export default function PositionMarker({
-	pos,
+	positions,
 	setShowDetailBox,
 	setDetailedPlace,
 	setShowShareBox,
 }) {
 	return (
-		<Marker position={pos.position}>
-			<Popup>
-				<div className="flex flex-col gap-2">
-					{pos.name && <span>{pos.name}</span>}
-					{pos.img && <img src={pos.img[0]} alt={pos.name} />}
-					<div className="flex items-center gap-1">
-						<Button
-							className={
-								"bg-blue-400 text-sm items-center transition-all duration-300 hover:bg-blue-900 hover:text-stone-100"
-							}
-							handleClick={() => {
-								setShowDetailBox(true);
-								setDetailedPlace(pos);
-							}}
-						>
-							See More
-						</Button>
-						<Button
-							className={
-								"bg-blue-400 text-sm items-center transition-all duration-300 hover:bg-blue-900 hover:text-stone-100"
-							}
-							handleClick={() => {
-								setDetailedPlace(pos);
-								setShowShareBox((showShareBox) => {
-									return { ...showShareBox, isOpen: true };
-								});
-							}}
-						>
-							share
-						</Button>
+		positions[0].length > 0 &&
+		positions[0].map((pos, key) => (
+			<Marker position={pos.position} key={key}>
+				<Tooltip tip={CustomTip}>{pos.name ? pos.name : pos.position}</Tooltip>
+				<Popup>
+					<div className="flex flex-col gap-2">
+						{pos.name && <span>{pos.name}</span>}
+						{pos.img && <img src={pos.img[0]} alt={pos.name} />}
+						<div className="flex items-center gap-1">
+							<Button
+								className={
+									"bg-blue-400 text-sm items-center transition-all duration-300 hover:bg-blue-900 hover:text-stone-100"
+								}
+								handleClick={() => {
+									setShowDetailBox(true);
+									setDetailedPlace(pos);
+								}}
+							>
+								See More
+							</Button>
+							<Button
+								className={
+									"bg-blue-400 text-sm items-center transition-all duration-300 hover:bg-blue-900 hover:text-stone-100"
+								}
+								handleClick={() => {
+									setDetailedPlace(pos);
+									setShowShareBox((showShareBox) => {
+										return { ...showShareBox, isOpen: true };
+									});
+								}}
+							>
+								share
+							</Button>
+						</div>
 					</div>
-				</div>
-			</Popup>
-		</Marker>
+				</Popup>
+			</Marker>
+		))
 	);
 }
