93cf2966ad4b0e0d71405e815b27ab8d8b23a32b
sgc93 <smachewgedefaw@gmail.com>
2024-03-22 08:34:05 -0900

    pass important setter functions to sidebar box component

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,13 +2,18 @@ import { useState } from "react";
 import MenuIcon from "./MenuIcon";
 import SideBarBox from "./SideBarBox";
 
-export default function SideBar({ setPosition }) {
+export default function SideBar({ setShowShareBox, setDetailedPlace }) {
 	const [showSideBar, setShowSideBar] = useState(false);
 
 	return (
 		<section className="flex flex-col gap-7 absolute top-4 left-4 z-[998]">
 			<MenuIcon showSideBar={showSideBar} setShowSideBar={setShowSideBar} />
-			{showSideBar && <SideBarBox setPosition={setPosition} />}
+			{showSideBar && (
+				<SideBarBox
+					setShowShareBox={setShowShareBox}
+					setDetailedPlace={setDetailedPlace}
+				/>
+			)}
 		</section>
 	);
 }
