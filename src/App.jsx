fdde921c71dc78de2c6ae09253879df2b819f05e
sgc93 <smachewgedefaw@gmail.com>
2024-03-22 11:51:41 -0900

    add new route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import MapPage from "./pages/MapPage";
 
 const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <HomePage />,
+	},
+	{
+		path: "map",
+		element: <MapPage />,
 	},
 	{
 		path: "*",
