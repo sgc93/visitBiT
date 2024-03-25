082935aafc916845684e471d416e8707de2158e5
sgc93 <smachewgedefaw@gmail.com>
2024-03-25 01:07:42 -0900

    handle btn clicks

--- src/pages/StoryTeller.jsx
+++ src/pages/StoryTeller.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { IoIosCloseCircle } from "react-icons/io";
+import { IoIosCloseCircle, IoMdArrowBack } from "react-icons/io";
 import Button from "../components/Button";
 import StoryMap from "../components/story/StoryMap";
 import { story } from "../services/bitStory";
@@ -7,6 +7,26 @@ import { story } from "../services/bitStory";
 export default function StoryTeller() {
 	const [isOpen, setIsOpen] = useState(true);
 	const [currStoryIndex, setCurrStoryIndex] = useState(0);
+
+	// handle close
+	function handleClosing(index) {
+		setIsOpen(false);
+	}
+
+	// handle next
+	function displayNext() {
+		if (currStoryIndex < story.length - 1) {
+			setCurrStoryIndex((currIndex) => currIndex + 1);
+		}
+	}
+
+	// handle previous
+	function displayPrevious() {
+		if (currStoryIndex > 0) {
+			setCurrStoryIndex((currIndex) => currIndex - 1);
+		}
+	}
+
 	return (
 		isOpen && (
 			<div className="storyTeller w-full h-full absolute top-0 left-0 flex justify-center">
@@ -23,7 +43,7 @@ export default function StoryTeller() {
 						</div>
 						<IoIosCloseCircle
 							className="text-2xl text-red-700 transition-all duration-300 hover:text-red-400"
-							onClick={() => setIsOpen(false)}
+							onClick={() => handleClosing()}
 						/>
 					</div>
 					<span className="h-[1px] bg-stone-600 mx-2"></span>
@@ -35,21 +55,50 @@ export default function StoryTeller() {
 							<StoryMap />
 						</div>
 					</div>
-					<div className="p-2 flex justify-end gap-2">
-						<Button
-							className={
-								"text-stone-500 border-2 border-stone-500 transition-all duration-300 hover:text-stone-400 hover:border-stone-400"
-							}
-						>
-							exit
-						</Button>
-						<Button
-							className={
-								"text-stone-900 bg-stone-500 border-2 border-stone-500 transition-all duration-300 hover:bg-stone-400 hover:border-stone-400"
-							}
-						>
-							next
-						</Button>
+					<div
+						className={`p-2 flex items-center ${
+							currStoryIndex > 0 ? "justify-between" : "justify-end"
+						}`}
+					>
+						{currStoryIndex > 0 && (
+							<Button
+								className={"bg-transparent border-none ml-1"}
+								handleClick={() => displayPrevious()}
+							>
+								<IoMdArrowBack className="text-stone-600 text-2xl translation-all duration-300 hover:text-stone-400" />
+							</Button>
+						)}
+						{currStoryIndex === story.length - 1 && (
+							<div className="text-sm ">
+								<span className=" text-stone-500">see more @ </span>
+								<a
+									href={story[currStoryIndex].link}
+									className="text-blue-900 transition-all duration-300 hover:text-blue-600"
+								>
+									{story[currStoryIndex].link}
+								</a>
+							</div>
+						)}
+						<div className="flex gap-2">
+							<Button
+								className={
+									"text-stone-500 border-2 border-stone-500 transition-all duration-300 hover:text-stone-400 hover:border-stone-400"
+								}
+								handleClick={() => handleClosing()}
+							>
+								exit
+							</Button>
+							{currStoryIndex < story.length - 1 && (
+								<Button
+									className={
+										"text-stone-900 bg-stone-500 border-2 border-stone-500 transition-all duration-300 hover:bg-stone-400 hover:border-stone-400"
+									}
+									handleClick={() => displayNext()}
+								>
+									next
+								</Button>
+							)}
+						</div>
 					</div>
 				</div>
 			</div>
