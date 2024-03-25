7c7f5c0a9cd2277a0d7727522589894ba1690a34
sgc93 <smachewgedefaw@gmail.com>
2024-03-25 10:48:37 -0900

    pass needed props

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import QuickSearch from "./QuickSearch";
 export default function Header({ dragConstraint, setMarkedPlace }) {
 	return (
 		<div className="absolute top-4 left-16 z-[999] flex items-start gap-2">
-			<QuickSearch />
+			<QuickSearch setMarkedPlace={setMarkedPlace} />
 			<NavBar dragConstraint={dragConstraint} setMarkedPlace={setMarkedPlace} />
 		</div>
 	);
