e0e3594c29a5c4f85a5ca62bd3d616d8ee5b098b
sgc93 <smachewgedefaw@gmail.com>
2024-03-25 02:03:03 -0900

    add descriptinve marker and tooltips for each position

--- src/components/story/StoryMap.jsx
+++ src/components/story/StoryMap.jsx
@@ -1,14 +1,22 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, TileLayer, Tooltip } from "react-leaflet";
+import FlyTo from "./FlyTo";
 
 const mapUrl = "http://{s}.google.com/vt?lyrs=s&x={x}&y={y}&z={z}";
 const subdomains = ["mt0", "mt1", "mt2", "mt3"];
-const pos = [11.597621756651337, 37.39551835806901];
 
-export default function StoryMap() {
+export default function StoryMap({ story }) {
 	return (
 		<div className="h-full w-full " id="map">
-			<MapContainer center={pos} zoom={15} className="h-full w-full">
+			<MapContainer
+				center={story.mark.center}
+				zoom={17}
+				className="h-full w-full"
+			>
 				<TileLayer url={mapUrl} subdomains={subdomains} />
+				<Marker position={story.mark.center}>
+					<Tooltip position={story.mark.center}>{story.topic}</Tooltip>
+				</Marker>
+				<FlyTo center={story.mark.center} />
 			</MapContainer>
 		</div>
 	);
