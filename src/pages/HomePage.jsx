795beefe6a3ca7c48e2b1abd27ed76841c26a600
sgc93 <smachewgedefaw@gmail.com>
2024-03-24 16:21:28 -0900

    display story telling window

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,20 @@
 import { useNavigate } from "react-router-dom";
 import Button from "../components/Button";
+import StoryTeller from "./StoryTeller";
 
 const pos = [11.597925500052817, 37.397444149178554];
 
 export default function HomePage() {
 	const navigateTo = useNavigate();
 	return (
-		<div className="w-screen h-screen glassmorphism flex flex-col gap-4 items-center justify-center">
-			<h1 className="text-5xl uppercase font-thin tracking-wide">visit BiT</h1>
-			<Button handleClick={() => navigateTo(`/map`)}>start exploring</Button>
-		</div>
+		<section className="w-screen h-screen glassmorphism">
+			<div className="w-full h-full flex flex-col gap-4 items-center justify-center">
+				<h1 className="text-5xl uppercase font-thin tracking-wide">
+					visit BiT
+				</h1>
+				<Button handleClick={() => navigateTo(`/map`)}>start exploring</Button>
+			</div>
+			<StoryTeller />
+		</section>
 	);
 }
