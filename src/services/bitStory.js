8a446014b0c20b8f93ae45682db6e91bc93eeebf
sgc93 <smachewgedefaw@gmail.com>
2024-03-25 03:20:41 -0900

    update srory content

--- src/services/bitStory.js
+++ src/services/bitStory.js
@@ -6,7 +6,7 @@ export const story = [
 			center: [11.587023661576392, 37.39566922187806],
 		},
 		content:
-			'Bahir Dar University (Amharic: ባሕር ዳር ዩኒቨርስቲ) is a university in the city of Bahir Dar, the capital of the Amhara Region in Ethiopia. The University is a combination of two smaller institutes formed earlier, after the departments were gradually raised to a degree level starting from 1996. The official slogan of the university is "Wisdom at the source of the Blue Nile". The University is composed of five colleges, four institutes, seven faculties, two academies and two schools',
+			'Bahir Dar University (Amharic: ባሕር ዳር ዩኒቨርስቲ) is a university in the city of Bahir Dar, the capital of the Amhara Region in Ethiopia. The University is a combination of two smaller institutes formed earlier, after the departments were gradually raised to a degree level starting from 1996. The official slogan of the university is "Wisdom at the source of the Blue Nile". The University is composed of five colleges, four institutes, seven faculties, two academies and two schools.',
 		link: "http://bdu.edu.et/",
 	},
 	{
