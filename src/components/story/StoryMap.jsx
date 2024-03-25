64d32157655be4d43e0a38e8404ac368bed77ec8
sgc93 <smachewgedefaw@gmail.com>
2024-03-25 00:29:41 -0900

    integrate leaflet map

--- src/components/story/StoryMap.jsx
+++ src/components/story/StoryMap.jsx
@@ -1,3 +1,15 @@
+import { MapContainer, TileLayer } from "react-leaflet";
+
+const mapUrl = "http://{s}.google.com/vt?lyrs=s&x={x}&y={y}&z={z}";
+const subdomains = ["mt0", "mt1", "mt2", "mt3"];
+const pos = [11.597621756651337, 37.39551835806901];
+
 export default function StoryMap() {
-	return <div>StoryMap</div>;
+	return (
+		<div className="h-full w-full " id="map">
+			<MapContainer center={pos} zoom={15} className="h-full w-full">
+				<TileLayer url={mapUrl} subdomains={subdomains} />
+			</MapContainer>
+		</div>
+	);
 }
