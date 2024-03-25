8543900fdc8366ca3a5291c715961d8b4f1c826a
sgc93 <smachewgedefaw@gmail.com>
2024-03-25 01:07:28 -0900

    add sample data about bit campus

--- src/services/bitStory.js
+++ src/services/bitStory.js
@@ -1,8 +1,16 @@
 export const story = [
 	{
-		topic: "BDU",
-		position: [],
+		topic: "Bahir Dar University",
+		mark: [11.597621756651337, 37.39551835806901],
 		content:
 			'Bahir Dar University (Amharic: ባሕር ዳር ዩኒቨርስቲ) is a university in the city of Bahir Dar, the capital of the Amhara Region in Ethiopia. The University is a combination of two smaller institutes formed earlier, after the departments were gradually raised to a degree level starting from 1996. The official slogan of the university is "Wisdom at the source of the Blue Nile". The University is composed of five colleges, four institutes, seven faculties, two academies and two schools',
+		link: "http://bdu.edu.et/",
+	},
+	{
+		topic: "Bahir Dar Institute of Technology",
+		mark: [11.597621756651337, 37.39551835806901],
+		content:
+			"Established in 1963, it's one of the oldest and most well-known public universities in Ethiopia for engineering and technology. BiT offers a wide range of programs, including undergraduate degrees, Master's degrees, and PhD programs in various engineering fields like Chemical and Food Engineering, Civil and Water Resource Engineering, and Mechanical and Industrial Engineering. ",
+		link: "https://bit.bdu.edu.et/",
 	},
 ];
