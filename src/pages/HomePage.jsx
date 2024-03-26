1e9995b677dd2f9cff3c96b2473de9fe31df2c44
sgc93 <smachewgedefaw@gmail.com>
2024-03-26 13:28:01 -0900

    create shareable state to start animation as soon as the user closes the story teller window

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import HomeContent from "../components/home/HomeContent";
 import StoryTeller from "./StoryTeller";
 
 export default function HomePage() {
+	const [isOpen, setIsOpen] = useState(true);
+
 	return (
 		<section className="home w-screen h-screen story-scroll ">
 			<div className="relative w-full h-full backdrop-blur-[4px] ">
-				<HomeContent />
-				<StoryTeller />
+				<HomeContent isOpen={isOpen} setIsOpen={setIsOpen} />
+				<StoryTeller isOpen={isOpen} setIsOpen={setIsOpen} />
 			</div>
 		</section>
 	);
