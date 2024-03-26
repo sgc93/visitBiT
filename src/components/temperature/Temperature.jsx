72b116847bb78bc8e675a012e014aa91e1f5bf86
sgc93 <smachewgedefaw@gmail.com>
2024-03-26 14:18:05 -0900

    create new component to show temperature

---
+++ src/components/temperature/Temperature.jsx
@@ -0,0 +1,7 @@
+export default function Temperature() {
+	return (
+		<section className="temp-overlay backdrop-blur-[4px] w-screen h-screen flex items-center justify-center">
+			Temperature
+		</section>
+	);
+}
