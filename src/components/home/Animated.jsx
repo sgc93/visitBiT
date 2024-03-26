8537b5fae9db1b23f2e755a20ffaeac75284f3b9
sgc93 <smachewgedefaw@gmail.com>
2024-03-26 10:19:12 -0900

    create a component for animated age teller text

---
+++ src/components/home/Animated.jsx
@@ -0,0 +1,12 @@
+export default function Animated() {
+	return (
+		<div className="flex gap-4 text-stone-100">
+			<span>since</span>
+			<div className="animated text-[18rem] font-bold tracking-wide">
+				<span>19</span>
+				<br />
+				<span>63</span>
+			</div>
+		</div>
+	);
+}
