72705f8c905572e5e2d0da7fb81691a9653b580c
sgc93 <smachewgedefaw@gmail.com>
2024-03-26 14:18:24 -0900

    update layout of homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,19 @@
 import { useState } from "react";
 import HomeContent from "../components/home/HomeContent";
+import Temperature from "../components/temperature/Temperature";
 import StoryTeller from "./StoryTeller";
 
 export default function HomePage() {
 	const [isOpen, setIsOpen] = useState(true);
 
 	return (
-		<section className="home w-screen h-screen story-scroll ">
-			<div className="home-overlay relative w-full h-full backdrop-blur-[4px] ">
+		<section className="overflow-x-hidden ">
+			<div className="home relative w-screen h-screen ">
 				<HomeContent isOpen={isOpen} setIsOpen={setIsOpen} />
-				<StoryTeller isOpen={isOpen} setIsOpen={setIsOpen} />
+			</div>
+			<StoryTeller isOpen={isOpen} setIsOpen={setIsOpen} />
+			<div className="temp relative w-screen h-screen ">
+				<Temperature />
 			</div>
 		</section>
 	);
