bb4ddf09c26b4562f867925c09615132e2fc40d5
sgc93 <smachewgedefaw@gmail.com>
2024-03-26 10:20:10 -0900

    update homepage contents and layouts

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,20 +1,13 @@
-import { useNavigate } from "react-router-dom";
-import Button from "../components/Button";
+import HomeContent from "../components/home/HomeContent";
 import StoryTeller from "./StoryTeller";
 
-const pos = [11.597925500052817, 37.397444149178554];
-
 export default function HomePage() {
-	const navigateTo = useNavigate();
 	return (
-		<section className="w-screen h-screen glassmorphism">
-			<div className="w-full h-full flex flex-col gap-4 items-center justify-center">
-				<h1 className="text-5xl uppercase font-thin tracking-wide">
-					visit BiT
-				</h1>
-				<Button handleClick={() => navigateTo(`/map`)}>start exploring</Button>
+		<section className="home w-screen h-screen story-scroll ">
+			<div className="relative w-full h-full backdrop-blur-[4px] ">
+				<HomeContent />
+				<StoryTeller />
 			</div>
-			<StoryTeller />
 		</section>
 	);
 }
