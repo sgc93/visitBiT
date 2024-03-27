fce859a7136d1966a6e69637d7f100774d2f4d89
sgc93 <smachewgedefaw@gmail.com>
2024-03-27 01:50:36 -0900

    update contents

--- src/components/story/Typing.jsx
+++ src/components/story/Typing.jsx
@@ -1,37 +1,9 @@
-import { useEffect, useRef } from "react";
-import Typed from "typed.js";
+import Typer from "../Typer";
 
 export default function Typing({ text }) {
-	const contentRef = useRef();
-
-	useEffect(() => {
-		const typed = new Typed("#typed-content", {
-			strings: [text],
-			typeSpeed: 10,
-			loop: false,
-		});
-
-		return () => typed.destroy();
-	}, [text]);
-
-	// scrolling down automatically while there is overflow
-	useEffect(() => {
-		const element = contentRef.current;
-		if (element) {
-			console.log(element);
-			console.log(element.scrollHeight, element.clientHeight);
-			if (element.clientHeight < element.scrollHeight) {
-				element.scrollTop = element.scrollHeight - element.clientHeight;
-			}
-		}
-	});
-
 	return (
-		<div
-			ref={contentRef}
-			className="w-2/4 h-full pr-2 flex flex-col text-stone-400 overflow-scroll overflow-x-hidden story-scroll"
-		>
-			<span id="typed-content"></span>
+		<div className="w-2/4 h-full pr-2 flex flex-col text-stone-400 overflow-scroll overflow-x-hidden story-scroll">
+			<Typer text={text} />
 		</div>
 	);
 }
