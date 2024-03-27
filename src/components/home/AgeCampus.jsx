d93f7f3f4cb7bbc462186e7eca970d2d893745d4
sgc93 <smachewgedefaw@gmail.com>
2024-03-26 22:25:10 -0900

    create new component to show historical origin of other campuses

---
+++ src/components/home/AgeCampus.jsx
@@ -0,0 +1,10 @@
+export default function AgeCampus() {
+	return (
+		<div className="w-2/3 h-full flex flex-col items-center justify-center gap-2">
+			<div className="top w-full h-1/2 bg-blue-300 rounded-r-xl rounded-t-xl">
+				age
+			</div>
+			<div className="bottom w-full h-1/2 bg-orange-300 rounded-xl"></div>
+		</div>
+	);
+}
