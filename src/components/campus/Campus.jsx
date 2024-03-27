16b9a181668bfbd42cc7e96e2cf0ff7a193dedd4
sgc93 <smachewgedefaw@gmail.com>
2024-03-27 01:48:52 -0900

    update components

--- src/components/campus/Campus.jsx
+++ src/components/campus/Campus.jsx
@@ -1,3 +1,18 @@
+import CampusData from "./CampusData";
+import CampusMap from "./CampusMap";
+import CampusNav from "./CampusNav";
+
 export default function Campus() {
-	return <div className="overlay w-fll h-full backdrop-blur-[4px]">Campus</div>;
+	return (
+		<div
+			id="campuses"
+			className="overlay w-fll h-full backdrop-blur-[4px] flex flex-col items-center gap-3 p-10"
+		>
+			<CampusNav />
+			<div className="flex w-full h-[95%]">
+				<CampusData />
+				<CampusMap />
+			</div>
+		</div>
+	);
 }
