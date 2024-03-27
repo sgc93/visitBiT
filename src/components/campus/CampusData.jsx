69a8c4a93d4899b5159eadf321303f8dc1101692
sgc93 <smachewgedefaw@gmail.com>
2024-03-27 06:34:16 -0900

    try with sample data

--- src/components/campus/CampusData.jsx
+++ src/components/campus/CampusData.jsx
@@ -1,3 +1,7 @@
-export default function CampusData() {
-	return <div className="w-1/2 h-full bg-orange-500">CampusData</div>;
+export default function CampusData({ campus }) {
+	return (
+		<div className="w-1/2 h-full py-4 px-7">
+			<div className="text-stone-500">name: {campus.name}</div>
+		</div>
+	);
 }
