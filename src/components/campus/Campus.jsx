3883fa9e9550f322f0261fd05bd9564772cb8593
sgc93 <smachewgedefaw@gmail.com>
2024-03-27 10:37:32 -0900

    handle navigating to next campus with navigating btn >

--- src/components/campus/Campus.jsx
+++ src/components/campus/Campus.jsx
@@ -9,6 +9,31 @@ export default function Campus() {
 	const [tabs, setTabs] = useState(campusTabs);
 	const [selectedTab, setSelectedTab] = useState(tabs[0]);
 
+	function handleNext() {
+		let updatedTab;
+		let currI;
+		const updatedTabs = tabs
+			.filter((tab, index) => {
+				if (tab.name === selectedTab.name) {
+					const nexTab = { ...tabs[index + 1], selected: true };
+					updatedTab = nexTab;
+					currI = index + 1;
+					return tab;
+				} else {
+					if (index !== currI) {
+						return tab;
+					}
+				}
+			})
+			.map((tab) => {
+				return { ...tab, selected: false };
+			});
+
+		updatedTabs.splice(currI, 0, updatedTab);
+		setTabs(updatedTabs);
+		setSelectedTab(updatedTab);
+	}
+
 	return (
 		<div
 			id="campuses"
@@ -18,11 +43,14 @@ export default function Campus() {
 				<CampusNav
 					tabs={tabs}
 					setTabs={setTabs}
-					selectedTab={selectedTab}
 					setSelectedTab={setSelectedTab}
 				/>
 				<div className="flex flex-col gap-1 w-[calc(100%-1rem)] h-full m-2 rounded-lg bg-stone-900">
-					<CampusWindowTab selectedTab={selectedTab} tabs={tabs} />
+					<CampusWindowTab
+						selectedTab={selectedTab}
+						tabs={tabs}
+						handleNext={handleNext}
+					/>
 					<div className="w-full h-full flex items-center gap-4">
 						<CampusData campus={selectedTab} />
 						<span className="bg-stone-900 h-[95%] w-[1px]"></span>
