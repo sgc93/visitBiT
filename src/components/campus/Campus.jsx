db2cf958bc188d69c492254a73329cb96e8ebe9f
sgc93 <smachewgedefaw@gmail.com>
2024-03-27 08:27:15 -0900

    update contents of current tab displayer for campus displayer window

--- src/components/campus/Campus.jsx
+++ src/components/campus/Campus.jsx
@@ -3,6 +3,7 @@ import { campusTabs } from "../../services/tabs";
 import CampusData from "./CampusData";
 import CampusMap from "./CampusMap";
 import CampusNav from "./CampusNav";
+import CampusWindowTab from "./CampusWindowTab";
 
 export default function Campus() {
 	const [tabs, setTabs] = useState(campusTabs);
@@ -13,34 +14,23 @@ export default function Campus() {
 			id="campuses"
 			className="overlay w-full h-full backdrop-blur-[4px] flex flex-col items-center gap-3 p-10"
 		>
-			<div className="flex flex-col gap-1 w-full h-full bg-stone-950 rounded-xl">
+			<div className="flex flex-col w-full h-full bg-stone-950 rounded-xl">
 				<CampusNav
 					tabs={tabs}
 					setTabs={setTabs}
 					selectedTab={selectedTab}
 					setSelectedTab={setSelectedTab}
 				/>
-				<CampusWindowTab selectedTab={selectedTab} />
-				<span className="h-[1px] w-full bg-stone-900"></span>
-				<div className="w-full h-full flex items-center gap-4">
-					<CampusData campus={selectedTab} />
-					<span className="bg-stone-900 h-[95%] w-[1px]"></span>
-					<CampusMap />
+				<div className="flex flex-col gap-1 w-[calc(100%-1rem)] h-full m-2 rounded-lg bg-stone-900 rounded-x">
+					<CampusWindowTab selectedTab={selectedTab} />
+					<span className="h-[1px] w-full bg-stone-900"></span>
+					<div className="w-full h-full flex items-center gap-4">
+						<CampusData campus={selectedTab} />
+						<span className="bg-stone-900 h-[95%] w-[1px]"></span>
+						<CampusMap />
+					</div>
 				</div>
 			</div>
-		</div>
-	);
-}
-
-function CampusWindowTab({ selectedTab }) {
-	return (
-		<div className="pl-4 py-2">
-			<span className="text-md text-stone-400 lowercase">
-				<span className="text-blue-600">&gt;</span>{" "}
-				<span className="text-stone-700">bahir Dar University </span>
-				<span className="text-blue-600">/ </span>
-				<span className="text-stone-600">{selectedTab.name}</span>
-			</span>
 		</div>
 	);
 }
