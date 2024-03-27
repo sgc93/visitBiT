5b6105e6d96a746fe910b9c26ddfabe9f57691cc
sgc93 <smachewgedefaw@gmail.com>
2024-03-27 09:01:07 -0900

    create setting tab

--- src/components/campus/CampusWindowTab.jsx
+++ src/components/campus/CampusWindowTab.jsx
@@ -1,21 +1,25 @@
+import { useState } from "react";
+import { FaToggleOn } from "react-icons/fa";
 import {
 	IoIosArrowBack,
 	IoIosArrowForward,
 	IoMdRefresh,
 	IoMdSettings,
 } from "react-icons/io";
+import { IoClose } from "react-icons/io5";
+import { RiEyeCloseLine } from "react-icons/ri";
 import Copy from "../Copy";
 
 export default function CampusWindowTab({ selectedTab }) {
 	return (
-		<div className="pl-4 py-2 w-full flex items-center gap-3">
-			<div className="w-full flex items-center gap-4">
+		<div className="pl-4 py-2 w-full flex items-center justify-between ">
+			<div className="w-[70%] flex items-center gap-4">
 				<div className="flex items-center gap-2 text-xl text-stone-400">
 					<IoIosArrowBack />
 					<IoIosArrowForward />
 					<IoMdRefresh />
 				</div>
-				<div className=" w-2/3 px-2 py-1 flex items-center justify-between text-md bg-stone-950 rounded-md lowercase">
+				<div className=" w-[80%] px-2 py-1 flex items-center justify-between text-md bg-stone-950 rounded-md lowercase">
 					<span>
 						<span className="text-stone-400">bahir Dar University </span>
 						<span className="text-blue-400">/ </span>
@@ -33,10 +37,43 @@ export default function CampusWindowTab({ selectedTab }) {
 	);
 }
 
+const className =
+	"text-xl text-stone-400 transition-all duration-300 hover:text-stone-300 cursor-pointer";
+
 function Setting() {
+	const [isOpen, setIsOpen] = useState(false);
+	const [isHidden, setIsHidden] = useState(false);
+
 	return (
-		<div className="relative pr-1">
-			<IoMdSettings className="text-xl text-stone-400 transition-all duration-300 hover:text-stone-300 cursor-pointer" />
+		<div className="w-[20%] relative pr-1 flex items-center justify-end gap-2">
+			{isOpen && (
+				<div className="absolute top-0 right-8 w-full h-44 p-2 bg-stone-950 text-stone-400 rounded-lg border-2 border-stone-800">
+					<div className="w-[calc(100% - 1.5rem)] px-1 flex items-center justify-between bg-stone-800 rounded-md transition-all duration-300 hover:bg-stone-700 cursor-pointer">
+						<div className="flex items-center gap-2 text-md ">
+							<RiEyeCloseLine />
+							<span>enable tab closing</span>
+						</div>
+						<FaToggleOn />
+					</div>
+					<div className="flex items-center gap-2">
+						<span>enable btn navigation</span>
+						<FaToggleOn />
+					</div>
+					<div className="flex items-center gap-2">
+						<span>enable refreshing</span>
+						<FaToggleOn />
+					</div>
+					<div className="flex items-center gap-2">
+						<span>hide tab bar</span>
+						<FaToggleOn />
+					</div>
+				</div>
+			)}
+			{!isOpen ? (
+				<IoMdSettings onClick={() => setIsOpen(true)} className={className} />
+			) : (
+				<IoClose onClick={() => setIsOpen(false)} className={className} />
+			)}
 		</div>
 	);
 }
