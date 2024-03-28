2a1ac988eed013f0d0ff22201a339037a1387342
sgc93 <smachewgedefaw@gmail.com>
2024-03-28 02:06:58 -0900

    add devider line between sections of homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,16 +8,19 @@ export default function HomePage() {
 	const [isOpen, setIsOpen] = useState(true);
 
 	return (
-		<section className="overflow-x-hidden ">
+		<section className="overflow-x-hidden flex flex-col ">
 			<div className="home relative w-screen h-screen ">
 				<HomeContent isOpen={isOpen} setIsOpen={setIsOpen} />
 			</div>
+			<span className="mx-2 h-1 bg-stone-300"></span>
 			<StoryTeller isOpen={isOpen} setIsOpen={setIsOpen} />
 			{!isOpen && (
 				<>
 					<div className="campus relative w-screen h-screen ">
 						<Campus />
 					</div>
+					<span className="mx-2 h-1 bg-stone-300"></span>
+
 					<div className="temp relative w-screen h-screen ">
 						<Weather />
 					</div>
