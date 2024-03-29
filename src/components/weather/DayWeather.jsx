e45761b540cee768081fabb32e7c07d088b41051
sgc93 <smachewgedefaw@gmail.com>
2024-03-29 00:46:28 -0900

    handle toggling between today and tomorrow

--- src/components/weather/DayWeather.jsx
+++ src/components/weather/DayWeather.jsx
@@ -1,39 +1,32 @@
-import { useState } from "react";
 import { dateFormatter } from "../../services/helpers";
 import LoaderBlock from "../LoaderBlock";
 
-export default function DayWeather({ forecastDay, isLoading, forecastTo }) {
+export default function DayWeather({
+	forecastDay,
+	isLoading,
+	forecastTo,
+	isToday,
+}) {
 	console.log(forecastDay);
 	return (
 		<div className="glassmorphism-white w-2/6 h-full rounded-lg p-2 flex flex-col gap-2">
-			<DayHeader forecastDay={forecastDay} isLoading={isLoading} />
+			<DayHeader
+				forecastDay={forecastDay}
+				isLoading={isLoading}
+				forecastTo={forecastTo}
+				isToday={isToday}
+			/>
 			<span className="h-[1px] bg-stone-700"></span>
 		</div>
 	);
 }
 
-function DayHeader({ forecastDay, isLoading, forecastTo }) {
-	const [isToday, setIsToday] = useState(true);
-
-	function today() {
-		if (!isToday) {
-			setIsToday(true);
-			forecastTo(0);
-		}
-	}
-
-	function tomorrow() {
-		if (isToday) {
-			setIsToday(false);
-			forecastTo(1);
-		}
-	}
-
+function DayHeader({ forecastDay, isLoading, forecastTo, isToday }) {
 	return (
 		<div className="flex items-center justify-between">
 			<div className="flex items-center gap-1">
 				<span className="text-blue-950 font-semibold">Daily Weather</span>
-				{isLoading ? (
+				{isLoading || !forecastDay ? (
 					<LoaderBlock width={"7rem"} height={"1.3rem"} />
 				) : (
 					<span className="w-28 flex justify-center underline underline-offset-2 text-sm font-semibold text-stone-600">
@@ -48,7 +41,7 @@ function DayHeader({ forecastDay, isLoading, forecastTo }) {
 							? "glassmorphism-white border-none"
 							: " transition-all duration-300 hover:border-none glassmorhpism-bg hover:glassmorphism-white"
 					}`}
-					onClick={() => today()}
+					onClick={() => forecastTo(0)}
 				>
 					today
 				</button>
@@ -58,7 +51,7 @@ function DayHeader({ forecastDay, isLoading, forecastTo }) {
 							? "glassmorphism-white border-none"
 							: " transition-all duration-300 hover:border-none glassmorphism-bg hover:glassmorphism-white"
 					}`}
-					onClick={() => tomorrow()}
+					onClick={() => forecastTo(1)}
 				>
 					tomorrow
 				</button>
