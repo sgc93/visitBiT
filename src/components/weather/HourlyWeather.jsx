524aed2f75af1fc11125de208058b6cef665d4f9
sgc93 <smachewgedefaw@gmail.com>
2024-03-29 09:43:32 -0900

    create component to display houly detailed weather data

---
+++ src/components/weather/HourlyWeather.jsx
@@ -0,0 +1,27 @@
+import { dateFormatter } from "../../services/helpers";
+import LoaderBlock from "../LoaderBlock";
+
+export default function HourlyWeather({ forecastDay, isLoading }) {
+	return (
+		<>
+			{isLoading ? (
+				<LoaderBlock width={"75%"} height={"100%"} />
+			) : (
+				<div className="w-[75%] h-full rounded-lg glassmorphism-white p-2">
+					<HourlyHeader forecastDay={forecastDay} />
+				</div>
+			)}
+		</>
+	);
+}
+
+function HourlyHeader({ forecastDay }) {
+	return (
+		<div className="flex items-center gap-3">
+			<span className="font-semibold text-blue-950">Hourly Weather Data</span>
+			<span className="text-sm font-semibold text-stone-400 underline underline-offset-2">
+				{dateFormatter(forecastDay.date)}
+			</span>
+		</div>
+	);
+}
