754e4e457451cb12821efc4a2f406ceed0718e6d
sgc93 <smachewgedefaw@gmail.com>
2024-03-28 15:58:37 -0900

    handle displaying header with formatted date

--- src/components/weather/Astronomy.jsx
+++ src/components/weather/Astronomy.jsx
@@ -1,3 +1,26 @@
+import { dateFormatter } from "../../services/helpers";
+import LoaderBlock from "../LoaderBlock";
+
 export default function Astronomy({ forecastDay, isLoading }) {
-	return <div className="w-1/6 h-full rounded-lg bg-blue-500">{isLoading}</div>;
+	return (
+		<>
+			{isLoading || !forecastDay ? (
+				<LoaderBlock width={"16%"} height={"100%"} />
+			) : (
+				<div className="glassmorphism-white w-1/6 h-full rounded-lg flex flex-col ">
+					<AstronomyHeader date={forecastDay.date} />
+					<span className="h-[1px] bg-stone-600 mx-2"></span>
+				</div>
+			)}
+		</>
+	);
+}
+
+function AstronomyHeader({ date }) {
+	return (
+		<div className="w-full h-[8%] flex items-center justify-between px-2">
+			<span className="text-blue-900 font-semibold capitalize">Astronomy</span>
+			<span className="text-stone-900 text-sm">{dateFormatter(date)}</span>
+		</div>
+	);
 }
