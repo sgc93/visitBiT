3ea34f708873307a4fa5334e2c6ebbae09579a5d
sgc93 <smachewgedefaw@gmail.com>
2024-03-29 10:58:35 -0900

    construct table and display hourly data

--- src/components/weather/HourlyWeather.jsx
+++ src/components/weather/HourlyWeather.jsx
@@ -1,4 +1,4 @@
-import { dateFormatter } from "../../services/helpers";
+import { dateFormatter, timeFormatter } from "../../services/helpers";
 import LoaderBlock from "../LoaderBlock";
 
 export default function HourlyWeather({ forecastDay, isLoading }) {
@@ -31,12 +31,51 @@ function HourlyDetail({ forecastDay }) {
 	const hour = forecastDay.hour;
 
 	return (
-		<div className="flex">
+		<div className="overflow-scroll table-scroll rounded-xl">
 			<DetailTable hourlyData={hour} />
 		</div>
 	);
 }
 
 function DetailTable({ hourlyData }) {
-	return <div>hourlyData</div>;
+	console.log(hourlyData);
+	return (
+		<table className="glassmorphism-bg rounded-xl w-full h-full">
+			<tr className="mx-2">
+				<th>time</th>
+				<th>temperature</th>
+				<th>condition</th>
+				<th>humidity</th>
+				<th>precipitation</th>
+				<th>pressure</th>
+				<th>wind</th>
+				<th>cloud</th>
+			</tr>
+			{hourlyData.map((data, key) => (
+				<tr key={key}>
+					<td>{timeFormatter(data.time)}</td>
+					<td>
+						<span>{data.temp_c}°c / </span>
+						<span>{data.temp_f}°f</span>
+					</td>
+					<td className="flex items-center gap-1">
+						<img
+							className="w-10"
+							src={data.condition.icon}
+							alt={data.condition.icon}
+						/>
+						<span>{data.condition.text}</span>
+					</td>
+					<td>{data.humidity}%</td>
+					<td>{data.precip_mm}mm</td>
+					<td>{data.pressure_mb}mb</td>
+					<td>
+						<span>{data.wind_dir} </span>
+						<span>{data.wind_kph}kpm</span>
+					</td>
+					<td>{data.cloud}%</td>
+				</tr>
+			))}
+		</table>
+	);
 }
