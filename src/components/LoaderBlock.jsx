bd060f5a3b8876801f26f6d2a8bd9295ad74735a
sgc93 <smachewgedefaw@gmail.com>
2024-03-29 09:43:08 -0900

    make its opacity to 50%

--- src/components/LoaderBlock.jsx
+++ src/components/LoaderBlock.jsx
@@ -2,7 +2,7 @@ export default function LoaderBlock({ width, height, className }) {
 	return (
 		<div
 			style={{ height: height, width: width }}
-			className={`loader-block glassmorphism-white my-1 rounded-lg ${className}`}
+			className={`loader-block glassmorphism-white opacity-50 my-1 rounded-lg ${className}`}
 		></div>
 	);
 }
