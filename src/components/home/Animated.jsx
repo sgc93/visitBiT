ba108246c9121243384de0c4c01788b9bd271029
sgc93 <smachewgedefaw@gmail.com>
2024-03-29 03:14:59 -0900

    restructuring animated age teller component

--- src/components/home/Animated.jsx
+++ src/components/home/Animated.jsx
@@ -1,7 +1,6 @@
-import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 
-export default function Animated({ isOpen, setIsOpen }) {
+export default function Animated({ isOpen }) {
 	const origin = 1963;
 	const year = new Date().getFullYear();
 	const a = year - origin;
@@ -30,51 +29,74 @@ export default function Animated({ isOpen, setIsOpen }) {
 	}, [isOpen, currYear, age]);
 
 	return (
-		<div className=" w-1/2 flex flex-col text-stone-100">
-			<div className="relative flex flex-col">
+		<div className=" w-1/2 h-full flex flex-col text-stone-100">
+			<div className="h-2/5 relative flex flex-col">
 				{year !== currYear && (
 					<span className="absolute top-10 left-5 text-2xl">since</span>
 				)}
 				<span className="year font-bold  ">{currYear}</span>
 			</div>
 			<div
-				className={`flex relative transition-all duration-500 ${
+				className={`flex relative transition-all duration-500 w-full h-3/5 ${
 					age > 0 ? "opacity-100" : "opacity-0"
 				}`}
 			>
-				<motion.span
-					animate={{ y: 0 }}
-					initial={{ y: 20 }}
-					transition={{ duration: 0.5, ease: "easeOut" }}
-					className=" year font-bold "
-				>
-					{age}{" "}
-				</motion.span>
-				<span className="absolute bottom-20 left-72 text-2xl transition-all duration-300">
-					<span
-						className={`age px-2 pb-10 mr-2 rounded-xl transition-all duration-500 ${
-							age > a / 3 ? "opacity-100 " : "opacity-0"
-						}`}
-					>
-						Evolved
+				<div className=" h-full flex flex-col items-center justify-end">
+					<div className="year font-bold mb-[-3rem]">{age}</div>
+				</div>
+				<div className="absolute bottom-[-1rem] left-72 h-full w-fit transition-all duration-300 flex gap-3">
+					<Attr
+						show={age > a / 3}
+						desc={"campuses schools institutes colleges"}
+						att={"Evolved"}
+						name={"age1"}
+					/>
+					<Attr
+						show={age > a / 2}
+						desc={"structured updated qualified education"}
+						att={"Proven"}
+						name={"age2"}
+					/>
+					<Attr
+						show={age > a / 1.1}
+						desc={"disciplined enlightened innovative students"}
+						att={"Fruitful"}
+						name={"age3"}
+					/>
+					<span className="flex flex-col items-center justify-end pb-10">
+						years
 					</span>
-					<span
-						className={`age px-2 pt-10 mr-2 rounded-xl transition-all duration-500 ${
-							age > a / 2 ? "opacity-100 " : "opacity-0"
-						}`}
-					>
-						Proven
-					</span>
-					<span
-						className={`age px-2 pb-10 mr-2 rounded-xl transition-all duration-500 ${
-							age > a / 1.1 ? "opacity-100 " : "opacity-0"
-						}`}
-					>
-						Fruitful
-					</span>
-					<span>years</span>
-				</span>
+				</div>
 			</div>
 		</div>
 	);
+}
+
+function Attr({ show, desc, att, name }) {
+	return (
+		<div
+			className={`w-1/4 h-full flex flex-col items-center justify-end gap-3 transition-all duration-500 ${
+				show ? "opacity-100 " : "opacity-0"
+			}`}
+		>
+			{name !== "age2" && <Holder name={name} />}
+			<div
+				className={`${name} rounded-xl w-full p-2 text-center text-sm text-stone-300 font-semibold`}
+			>
+				{desc}
+			</div>
+			<span
+				className={`${name} text-center text-2xl px-2 ${
+					name === "age2" ? "pt-10" : "pb-10"
+				} w-full rounded-xl  `}
+			>
+				{att}
+			</span>
+			{name === "age2" && <Holder name={name} />}
+		</div>
+	);
+}
+
+function Holder({ name }) {
+	return <div className={`w-full h-8 rounded-xl opacity-[.24] ${name}`}></div>;
 }
