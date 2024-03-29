3c3e89b7c49458908284cc094fa6f46bd2ea39c8
sgc93 <smachewgedefaw@gmail.com>
2024-03-28 15:16:40 -0900

    display current weather condition

--- src/components/weather/CurrentWeather.jsx
+++ src/components/weather/CurrentWeather.jsx
@@ -1,71 +1,135 @@
+import { MdOutlineNightlightRound, MdSunny } from "react-icons/md";
 import { dateFormatter, timeFormatter } from "../../services/helpers";
 
-export default function CurrentWeather() {
-	const current = {
-		cloud: 0,
-		condition: {
-			text: "Clear ",
-			icon: "//cdn.weatherapi.com/weather/64x64/night/113.png",
-			code: 1000,
-		},
-		feelslike_c: 24.1,
-		feelslike_f: 75.3,
-		gust_kph: 25.4,
-		humidity: 26,
-		is_day: 0,
-		last_updated: "2000-11-30 17:00",
-
-		precip_in: 0,
-		precip_mm: 0,
-		pressure_in: 29.88,
-		pressure_mb: 1012,
-		temp_c: 24.5,
-		temp_f: 76,
-		uv: 1,
-		vis_km: 10,
-		vis_miles: 6,
-		wind_degree: 293,
-		wind_dir: "WNW",
-		wind_kph: 14.4,
-		wind_mph: 8.9,
-	};
+export default function CurrentWeather({ current, isLoading }) {
 	return (
 		<div className="w-2/6 h-full rounded-lg glassmorphism-white flex flex-col">
-			<Title />
-			<span className="w-full h-[1px] bg-stone-700"></span>
-			<div className="w-full h-3/6 bg-orange-500"></div>
-			<div className="w-full h-[42%] bg-blue-500"></div>
+			<Title current={current} isLoading={isLoading} />
+			<span className=" h-[1px] bg-stone-700 mx-2"></span>
+			<Temperature current={current} isLoading={isLoading} />
+			<CurrentDetail current={current} isLoading={isLoading} />
 		</div>
 	);
 }
 
-function Title({ updateTime }) {
+function Title({ current, isLoading }) {
 	let date, time;
-	if (updateTime) {
-		date = dateFormatter(updateTime);
-		time = timeFormatter(updateTime);
+	if (!isLoading && current) {
+		date = dateFormatter(current.last_updated);
+		time = timeFormatter(current.last_updated);
 	}
 
 	return (
-		<div className="w-full h-[8%] flex items-center justify-between px-2 ">
+		<div className="w-full h-[12%] flex items-center justify-between px-2 ">
 			<span className="text-blue-950 font-semibold">Current weather</span>
-			{updateTime ? (
-				<div className="flex flex-col">
-					<span>{time}</span>
-					<span>{date}</span>
-				</div>
+			<div className="flex flex-col justify-center w-[25%] h-full ">
+				{isLoading || !current ? (
+					<LoaderBlock width={"100%"} height={"100%"} />
+				) : (
+					<>
+						<span className="text-sm font-semibold text-stone-950">{time}</span>
+						<span className="text-sm font-thin text-stone-950">{date}</span>
+					</>
+				)}
+			</div>
+		</div>
+	);
+}
+
+function Temperature({ current, isLoading }) {
+	return (
+		<div className="w-full h-[40%] flex gap-2 p-2">
+			{isLoading || !current ? (
+				<LoaderBlock width={"50%"} height={"100%"} />
 			) : (
-				<LoaderBlock width={"100%"} height={"100%"} />
+				<div className="glassmorphism-bg rounded-lg relative flex flex-col items-center w-[50%] h-full py-3 px-1 overflow-hidden">
+					<img
+						src={current.condition.icon}
+						alt={current.condition.text}
+						className=" h-full w-full translate-y-[-1.3rem] "
+					/>
+					<div className="absolute bottom-2  flex items-end gap-1">
+						<span className="text-5xl text-stone-900 font-semibold">
+							{current.temp_c}°C
+						</span>
+						<span className="text-lg font-semibold text-stone-800">
+							{current.temp_f}°F
+						</span>
+					</div>
+				</div>
+			)}
+			{isLoading || !current ? (
+				<LoaderBlock width={"50%"} height={"100%"} />
+			) : (
+				<div className="glassmorphism-bg rounded-lg w-1/2 h-full flex flex-col items-center p-2">
+					<span className="text-blue-900 text-4xl ">
+						{current.condition.text}
+					</span>
+					<span className="text-stone-900 flex items-center">
+						{current.is_day ? (
+							<>
+								<span>day</span>
+								<MdSunny />
+							</>
+						) : (
+							<>
+								<span>night</span>
+								<MdOutlineNightlightRound />
+							</>
+						)}
+					</span>
+				</div>
 			)}
 		</div>
 	);
 }
 
-function LoaderBlock({ width, height }) {
+function CurrentDetail({ current, isLoading }) {
+	let detail;
+	if (!isLoading && current) {
+		detail = {
+			cloud: `${current.cloud > 0 ? `${current.cloud}%` : "No Cloud "}`,
+			humidity: `${current.humidity}%`,
+			precipitation: `${
+				current.precip_mm > 0 ? `${current.precip_mm} mm` : "No precipitation "
+			}`,
+			pressure: `${current.pressure_mb} mb`,
+			ultraViolet: current.uv,
+			wind: `${current.wind_dir} ${current.wind_kph} kph`,
+		};
+	}
+
+	return (
+		<>
+			{isLoading || !current ? (
+				<LoaderBlock width={"97%"} height={"48%"} className={"mx-2 p-3 mb-2"} />
+			) : (
+				<div className="glassmorphism-bg rounded-lg mx-2 p-3 mb-2 h-[48%] overflow-hidden flex flex-col items-center gap-[.2rem]">
+					{Object.keys(detail).map((key) => (
+						<CurDet key={key} index={key} data={detail[key]} />
+					))}
+				</div>
+			)}
+		</>
+	);
+}
+
+function CurDet({ data, index }) {
+	return (
+		<div className="w-5/6 flex items-center justify-between glassmorphism-bg rounded-md px-2 py-[1.2px]">
+			<span className="capitalize text-sm font-semibold">{index}</span>
+			<span className=" w-1/2 text-start text-sm lowercase font-bold">
+				{data}
+			</span>
+		</div>
+	);
+}
+
+function LoaderBlock({ width, height, className }) {
 	return (
 		<div
 			style={{ height: height, width: width }}
-			className="loader-block bg-stone-800"
+			className={`loader-block glassmorphism-white my-1 rounded-lg ${className}`}
 		></div>
 	);
 }
