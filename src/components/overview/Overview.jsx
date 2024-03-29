4b980a3675fe229348960e9c9d58d4d731744743
sgc93 <smachewgedefaw@gmail.com>
2024-03-29 13:53:27 -0900

    handle controlling overview map in s separate component

--- src/components/overview/Overview.jsx
+++ src/components/overview/Overview.jsx
@@ -1,29 +1,13 @@
 import { useState } from "react";
+import Controller from "./MapController";
 import OverviewMap from "./OverviewMap";
 
 export default function Overview() {
 	const [isOpen, setIsOpen] = useState(true);
 	return (
-		<div className="glassmorphism-bg w-full h-screen backdrop-blur-[8px] flex gap-8 p-6">
+		<div className="glassmorphism-bg w-full h-screen backdrop-blur-[8px] flex flex-col gap-8 p-6">
 			<Controller isOpen={isOpen} setIsOpen={setIsOpen} />
 			{isOpen && <OverviewMap />}
-		</div>
-	);
-}
-
-function Controller({ isOpen, setIsOpen }) {
-	function handleToggling() {
-		setIsOpen((isOpen) => !isOpen);
-	}
-	return (
-		<div className="flex items-start gap-2">
-			<span className="text-stone-400 font-semibold ">controller</span>
-			<button
-				onClick={() => handleToggling()}
-				className="glassmorphism-white px-2 rounded-lg transition-all duration-300 text-blue-950 font-semibold hover:bg-stone-300  hover:translate-y-[-2px]"
-			>
-				{isOpen ? "hide map" : "show overview map"}
-			</button>
 		</div>
 	);
 }
