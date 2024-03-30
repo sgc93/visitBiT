3df5aebd40582fceb959f8f270215f48c621d607
sgc93 <smachewgedefaw@gmail.com>
2024-03-29 23:27:06 -0900

    make overview have its own height

--- src/components/overview/Overview.jsx
+++ src/components/overview/Overview.jsx
@@ -5,7 +5,7 @@ import OverviewMap from "./OverviewMap";
 export default function Overview() {
 	const [isOpen, setIsOpen] = useState(true);
 	return (
-		<div className="glassmorphism-bg w-full h-screen backdrop-blur-[8px] flex flex-col gap-8 p-6">
+		<div className="glassmorphism-bg w-full backdrop-blur-[8px] flex flex-col gap-8 p-6">
 			<Controller isOpen={isOpen} setIsOpen={setIsOpen} />
 			{isOpen && <OverviewMap />}
 		</div>
