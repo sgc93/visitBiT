111843955289ef92ee6d8f57c3931840fed3d29f
sgc93 <smachewgedefaw@gmail.com>
2024-03-30 01:53:51 -0900

    create a simulator component to handle simulating

---
+++ src/components/id/Simulator.jsx
@@ -0,0 +1,14 @@
+import EmployeeId from "./EmployeeId";
+import StudentId from "./StudentId";
+
+export default function Simulator({ type, setIsSimulatorOpened }) {
+	const studentId = type === "STUDENT ID";
+	const employeeId = type === "EMPLOYEE ID";
+
+	return (
+		<div className="w-full h-full flex items-center justify-center backdrop-blur-[5px]">
+			{studentId && <StudentId setIsOpen={setIsSimulatorOpened} />}
+			{employeeId && <EmployeeId setIsOpen={setIsSimulatorOpened} />}
+		</div>
+	);
+}
