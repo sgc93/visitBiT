5d9aaab33b591394b78dfd8588e7bae725ecca04
sgc93 <smachewgedefaw@gmail.com>
2024-03-29 23:26:19 -0900

    handle opening modal windows

--- src/components/home/HomeContent.jsx
+++ src/components/home/HomeContent.jsx
@@ -4,24 +4,42 @@ import { homeTabs } from "../../services/tabs";
 import Button from "../Button";
 import Animated from "./Animated";
 
-export default function HomeContent({ isOpen, setIsOpen }) {
+export default function HomeContent({
+	isBoxOpened,
+	setIsHintBoxOpened,
+	setIsIdBoxOpened,
+}) {
 	return (
 		<div className="overlay  w-full h-full flex items-center p-20">
-			<NavContent setIsOpen={setIsOpen} />
-			<Animated isOpen={isOpen} setIsOpen={setIsOpen} />
+			<NavContent
+				setIsHintBoxOpened={setIsHintBoxOpened}
+				setIsIdBoxOpened={setIsIdBoxOpened}
+			/>
+			<Animated isOpen={isBoxOpened} />
 		</div>
 	);
 }
 
-function NavContent({ setIsOpen }) {
-	function openHitBox() {
-		setIsOpen(true);
+function NavContent({ setIsHintBoxOpened, setIsIdBoxOpened }) {
+	function openHintBox() {
+		setIsHintBoxOpened(true);
 	}
+
+	function openIdSimulator() {
+		setIsIdBoxOpened(true);
+	}
+
 	return (
 		<div className="w-1/2 h-full flex flex-col justify-between">
 			<ul className="list ml-5 text-stone-300 flex flex-col gap-2">
 				{homeTabs.map((tab, key) => (
-					<Tab tab={tab} key={key} index={key} openHitBox={openHitBox} />
+					<Tab
+						tab={tab}
+						key={key}
+						index={key}
+						openHintBox={openHintBox}
+						openIdSimulator={openIdSimulator}
+					/>
 				))}
 			</ul>
 			<Hero />
@@ -29,11 +47,13 @@ function NavContent({ setIsOpen }) {
 	);
 }
 
-function Tab({ tab, index, openHitBox }) {
+function Tab({ tab, index, openHintBox, openIdSimulator }) {
 	const [isOpen, setIsOpen] = useState(false);
 	function handleClick() {
 		if (index === 0) {
-			openHitBox();
+			openHintBox();
+		} else if (index === 1) {
+			openIdSimulator();
 		}
 	}
 	return (
